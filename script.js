0edb626a887dc0dce29c1ff9afc4edcafb20d97e
kjdcompany <47058305+kjdcompany@users.noreply.github.com>
2019-12-03 23:57:42 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,26 @@
+//Make the menu appear on clicking the burger
 const bruger = document.querySelector(".burger-menu");
-const body = document.getElementById("motherland");
+const header = document.getElementById("headerH");
 const mobileMenu = document.getElementById("mobile-menu");
+const mainBlock = document.getElementById("mainM");
+const footer = document.getElementById("footerF");
 
 bruger.addEventListener("click", () => {
-  bruger.classList.toggle("rotate");
-  body.classList.toggle("no-overflow");
   mobileMenu.classList.toggle("nodisplay");
+  bruger.classList.toggle("rotate");
+  header.classList.toggle("full-screen");
+  mobileMenu.classList.toggle("slide-in");
+  mainBlock.classList.toggle("no");
+  footer.classList.toggle("no");
 });
+
+//Change the bottom color of the inputs
+const errorMessage = el => {
+  el.setCustomValidity("Запольните пожалуйста эту ячейку");
+  el.style.borderBottom = "1px solid red";
+};
+
+const typing = el => {
+  el.setCustomValidity("");
+  el.style.borderBottom = "";
+};
